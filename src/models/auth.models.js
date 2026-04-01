79c61801ca54d75ff0140034f0eed61f7402e160
Arif14377 <arifrahman14377@gmail.com>
2026-04-01 18:19:29 +0700

    fix: hash password

--- src/models/auth.models.js
+++ src/models/auth.models.js
@@ -1,5 +1,6 @@
 import * as db from '../../db/index.js'
 import * as hash from '../lib/hash.js'
+import crypto from "crypto"
 
 export async function register(dataUser) {
     // cek db apakah email sudah terdaftar
@@ -7,23 +8,25 @@ export async function register(dataUser) {
         SELECT email FROM users WHERE email = $1
     `
 
-    const res = await db.query(textQuery1, dataUser.email )
+    const res = await db.query(textQuery1, [dataUser.email] )
+    console.log(res)
 
     // Jika sudah terdaftar, throw error
-    if (res.rowCount == 0) {
+    if (res.rowCount == 1) {
         throw new Error("Email sudah terdaftar.")
     }
 
     // Jika belum terdaftar, hash password
-    const hashPassword = hash.generateHash(dataUser.password)
+    const hashPassword = await hash.generateHash(dataUser.password)
+    console.log("hashPassword: ", hashPassword)
 
     // simpan data user ke db
     const textQuery2 = `
-        INSERT INTO users(full_name, email, password)
-        VALUES('$1', '$2', '$3')
+        INSERT INTO users(id, full_name, email, password)
+        VALUES($1, $2, $3, $4)
     `
-
-    await db.query(textQuery2, dataUser.fullName, dataUser.email, dataUser.password)
+    const userId = crypto.randomUUID()
+    await db.query(textQuery2, [userId, dataUser.fullName, dataUser.email, hashPassword])
     
     const results = {
         message: "Registrasi berhasil",
