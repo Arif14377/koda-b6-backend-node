2e56ea8d80f2abea97042bb38afdc37eca2e00fa
Arif14377 <arifrahman14377@gmail.com>
2026-04-01 23:41:02 +0700

    feat: create structur auth path

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -4,5 +4,9 @@ import * as authController from '../controllers/auth.controllers.js'
 const authRouter = Router()
 
 authRouter.post('/register', authController.register)
+authRouter.post('/login', authController.login)
+authRouter.post('/forgot-password', authController.generateOTP)
+authRouter.post('/forgot-password/verifikasi-otp', authController.verifikasiOTP)
+authRouter.post('/forgot-password/changePassword', authController.changePassword)
 
 export default authRouter
