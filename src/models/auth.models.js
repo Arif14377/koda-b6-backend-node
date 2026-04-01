e4cce4bb21c12e40c6aa4bc607957eadef4258e2
Arif14377 <arifrahman14377@gmail.com>
2026-04-01 16:43:50 +0700

    feat: create register controller

--- src/models/auth.models.js
+++ src/models/auth.models.js
@@ -1,13 +1,13 @@
 import * as db from '../../db/index.js'
 import * as hash from '../lib/hash.js'
 
-export async function register(bodyRequest) {
+export async function register(dataUser) {
     // cek db apakah email sudah terdaftar
     const textQuery1 = `
         SELECT email FROM users WHERE email = $1
     `
 
-    const res = await db.query(textQuery1, bodyRequest.email )
+    const res = await db.query(textQuery1, dataUser.email )
 
     // Jika sudah terdaftar, throw error
     if (res.rowCount == 0) {
@@ -15,7 +15,7 @@ export async function register(bodyRequest) {
     }
 
     // Jika belum terdaftar, hash password
-    const hashPassword = hash.generateHash(bodyRequest.password)
+    const hashPassword = hash.generateHash(dataUser.password)
 
     // simpan data user ke db
     const textQuery2 = `
@@ -23,7 +23,7 @@ export async function register(bodyRequest) {
         VALUES('$1', '$2', '$3')
     `
 
-    await db.query(textQuery2, bodyRequest.fullName, bodyRequest.email, bodyRequest.password)
+    await db.query(textQuery2, dataUser.fullName, dataUser.email, dataUser.password)
     
     const results = {
         message: "Registrasi berhasil",
