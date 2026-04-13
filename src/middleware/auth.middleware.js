a156f8a2c83a07073046391edb87f2fe3b44d2b1
Arif14377 <arifrahman14377@gmail.com>
2026-04-13 23:18:12 +0700

    feat: add auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken'
+
+export function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization']
+
+    if (!authHeader) {
+        console.error("Authorization required.")
+        res.statusCode = 401
+        res.json({
+            success: false,
+            error: "Access is denied due to invalid credentials."
+        })
+        return
+    }
+
+    const parts = authHeader.split(' ')
+        if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        console.error("Invalid token format.")
+        res.statusCode = 401
+        res.json({
+            success: false,
+            error: "Access is denied due to invalid credentials."
+        })
+        return
+    }
+
+    const tokenString = parts[1]
+
+    try {
+        const decoded = jwt.verify(tokenString, process.env.JWT_SECRET)
+        req.userId = decoded.userId
+        next()
+    } catch (error) {
+        console.error("invalid or expired token")
+        res.statusCode = 401
+        res.json({
+            success: false,
+            error: "Access is denied due to invalid credentials."
+        })
+    }
+}
