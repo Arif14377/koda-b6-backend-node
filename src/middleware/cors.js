71b5ef19b95446c8289c46b1ec7139941a7be1ee
Arif14377 <arifrahman14377@gmail.com>
2026-04-16 02:36:47 +0700

    fix: cors origin

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,8 @@
 import cors from 'cors'
 
 const corsOptions = {
-    // origin: 'http://68.183.226.223:20101'
-    origin: 'http://localhost:5173'
+    origin: 'http://68.183.226.223:20201'
+    // origin: 'http://localhost:5173'
 }
 
 export default cors(corsOptions)
