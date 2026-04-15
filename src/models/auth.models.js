85f14040991c30a97a6a627a5a573a7b4dc04a3c
Arif14377 <arifrahman14377@gmail.com>
2026-04-15 17:58:19 +0700

    feat: add user role to jwt and response client

--- src/models/auth.models.js
+++ src/models/auth.models.js
@@ -68,6 +68,7 @@ export async function login(dataLogin) {
         id: data.id,
         fullName: data.full_name,
         email: data.email,
+        roleId: data.role_id
     }
 
     const isPasswordTrue = await hash.verifyHash(data.password, dataLogin.password)
@@ -82,7 +83,7 @@ export async function login(dataLogin) {
     // jika benar -> re        // const dataResponse = turn 200 + data
     if (isPasswordTrue) {
         // create jwt
-        const token = jwt.sign({userId: data.id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        const token = jwt.sign({userId: data.id, userRole: data.role_id}, process.env.JWT_SECRET, {expiresIn: '1h'})
 
         return {
             message: "Successfully loged in.",
